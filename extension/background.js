b312c92c835ef2bb0c2317328a12912da5ed28f9
Anshul Saha <anshulsaha15@gmail.com>
2021-08-07 23:36:41 +0530

    extension

--- extension/background.js
+++
@@ -1,9 +0,0 @@
-if (window.location.href.includes('https://www.amazon.in' || 'https://amazon.in')) {
-    const productName = document.getElementById('productTitle').innerText
-    const productPrice = document.getElementById('priceblock_dealprice').innerText
-    if (productName && productPrice) {
-        console.log(productName, productPrice)
-    } else {
-        console.log("nah")
-    }
-}
--- extension/popup.js
+++ extension/popup.js
@@ -1,3 +1,46 @@
+const formatName = (productName) => {
+    if (productName.includes('(')) {
+        productName = productName.split('(')[0]
+    }
+    if (productName.split(' ').length > 3) {
+        let newProductName = ''
+        productName.split(' ').forEach((word, index) => {
+            if (index < 3) {
+                newProductName += word + ' '
+            }
+        })
+        return newProductName
+    }
+    return productName
+}
+
 window.onload = () => {
-    document.getElementById('hi').innerText = 'hello'
+    chrome.tabs.query({currentWindow: true, active: true}, (tabs) => { 
+        const url = tabs[0].url 
+        if (url.includes('https://www.amazon.in' || 'https://amazon.in')) {
+            chrome.tabs.executeScript({
+                code: 'document.getElementById("productTitle").innerHTML'
+            }, (results) => {
+                if (results[0]) {
+                    const productName = results[0].trim()
+                    document.getElementById('productName').innerText = formatName(productName) + '...'
+                    fetch(`https://kaufer.anshulsaha.repl.co/yt?productName=${productName}`)
+                    .then(async (resp) => {
+                        const response = await resp.json()
+                        alert(response)
+                    })
+                    .catch(err => alert(err))
+                } else {
+                    document.getElementById('productName').innerText = 'details will show up only on an amazon product page'
+                }
+            });
+            chrome.tabs.executeScript({
+                code: 'document.getElementById("priceblock_dealprice").innerHTML'
+            }, (results) => {
+                if (results[0]) {
+                    document.getElementById('productPrice').innerText = results[0].trim();
+                }
+            });         
+        }
+    });
 }
